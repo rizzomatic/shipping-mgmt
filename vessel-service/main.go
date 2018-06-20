1267a34723a208ecbc192b5c519c5b2b412b9bc2
Mike Rizzo <rizzomatic@gmail.com>
2018-06-19 20:36:45 -0400

    initial commit

---
+++ vessel-service/main.go
@@ -0,0 +1,69 @@
+// vessel-service/main.go
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	pb "github.com/rizzomatic/shipping-mgmt/vessel-service/proto/vessel"
+	"github.com/micro/go-micro"
+)
+
+type Repository interface {
+	FindAvailable(*pb.Specification) (*pb.Vessel, error)
+}
+
+type VesselRepository struct {
+	vessels []*pb.Vessel
+}
+
+// FindAvailable - checks a specification against a map of vessels,
+// if capacity and max weight are below a vessels capacity and max weight,
+// then return that vessel.
+func (repo *VesselRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel, error) {
+	for _, vessel := range repo.vessels {
+		if spec.Capacity <= vessel.Capacity && spec.MaxWeight <= vessel.MaxWeight {
+			return vessel, nil
+		}
+	}
+	return nil, errors.New("No vessel found that matches that spec.")
+}
+
+//grpc service handler
+type service struct {
+	repo Repository
+}
+
+func (s *service) FindAvailable(ctx context.Context, req *pb.Specification, res *pb.Response) error {
+	//find the next available vessel
+	vessel, err := s.repo.FindAvailable(req)
+	if err != nil {
+		return err
+	}
+
+	//now set the found vessel as part of the repsonse message type
+	res.Vessel = vessel 
+	return nil
+}
+
+func main() {
+	vessels := []*pb.Vessel{
+		&pb.Vessel{Id: "vessel001", Name: "Boaty McBoatface", MaxWeight: 200000, Capacity: 500},
+	}
+	repo := &VesselRepository{vessels}
+
+	srv := micro.NewService(
+		micro.Name("go.micro.srv.vessel"),
+		micro.Version("latest"),
+	)
+
+	srv.Init()
+
+	//Register the service
+	pb.RegisterVesselServiceHandler(srv.Server(), &service{repo})
+
+	if err := srv.Run(); err != nil {
+		fmt.Println(err)
+	}
+}
